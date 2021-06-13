b5fb87c4ee8a640aadc60e7e9b88a318b6d697d2
ryoo14 <anana12185@gmail.com>
2021-06-13 18:26:54 +0900

    add subcommand root and list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "ryoo14 <anana12185@gmail.com")]
+struct Opts {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    Root(Root),
+    List(List),
+}
+
+#[derive(Clap)]
+// print root directory
+struct Root {
+}
+
+#[derive(Clap)]
+// print working directory list
+struct List {
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    match opts.subcmd {
+        SubCommand::Root(r) => println!("root"),
+        SubCommand::List(l) => println!("list"),
+    }
+    
 }
