bc04fa62ffa35e294615141347d8b9da543a5d42
ryoo14 <anana12185@gmail.com>
2021-12-18 18:46:21 +0900

    version up clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use clap::Clap;
+use clap::{Parser, Subcommand};
 use wh::WorkHub;
 use anyhow::Result;
 
-#[derive(Clap)]
+#[derive(Parser)]
 #[clap(
     version = "0.3.0",
     author = "ryoo14 <anana12185@gmail.com",
@@ -13,68 +13,42 @@ struct Opts {
     subcmd: SubCommand,
 }
 
-#[derive(Clap)]
+#[derive(Subcommand)]
 enum SubCommand {
-    #[clap(about = "Prints root dir")]
-    Root(Root),
-    #[clap(about = "Prints working dir list")]
-    List(List),
-    #[clap(about = "Create working dir")]
-    Create(Create),
-    #[clap(about = "Clone Github repository")]
-    Get(Get),
-}
-
-#[derive(Clap)]
-// print root directory
-struct Root {
-}
-
-#[derive(Clap)]
-// print working directory list
-struct List {
-}
-
-#[derive(Clap)]
-// create working directory
-struct Create {
-    targetdir_path: String,
-}
-
-#[derive(Clap)]
-// clone github repository
-struct Get {
-    repository: String,
+    Root{},
+    List{},
+    Create{ targetdir_path: String },
+    Get{ repository: String },
 }
 
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
     let wh = WorkHub::new()?;
     
-    match opts.subcmd {
-        SubCommand::Root(_) => { 
+    match &opts.subcmd {
+        SubCommand::Root{} => { 
             println!("{}", wh.root);
         },
-        SubCommand::List(_) => {
+        SubCommand::List{} => {
             let workdir_list = wh.list()?;
             for workdir in workdir_list {
                 println!("{}", workdir);
             }
         }
-        SubCommand::Create(c) => {
-            match wh.create(&c.targetdir_path) {
+        SubCommand::Create{ targetdir_path } => {
+            match wh.create(targetdir_path) {
                 Ok(_) => {
-                    println!("Successfully created {}", &c.targetdir_path);
+                    println!("Successfully created {}", targetdir_path);
                 },
                 Err(e) => {
                     println!("{}", e);
                 },
             }
         },
-        SubCommand::Get(g) => {
-            match wh.get(&g.repository) {
+        SubCommand::Get{ repository } => {
+            match wh.get(repository) {
                 Ok(_) => {
-                    println!("Successfully cloned {}", &g.repository);
+                    println!("Successfully cloned {}", repository);
                 },
                 Err(e) => {
                     println!("{}", e);
